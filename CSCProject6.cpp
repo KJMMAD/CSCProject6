c97a36c9c531180dbefed01bbb07175cfce15359
KJMMAD <keira_jmmadrid.@yahoo.com>
2025-12-12 17:22:42 -0500

    Added code a did some minor testing to see if it works.

--- CSCProject6.cpp
+++ CSCProject6.cpp
@@ -49,16 +49,53 @@ void showLowest(string, int);
 
 int main()
 {
+    string lowestReg;
+    string regName[5]; //Array only has room for 5 possible names given the city has 5 regions - North, East, South, West, and Central 
 
+    int lowestAcc;
+    int accNum[5]; //Array has room for 5 possible numbers to match to the corresponding region
+
+
+    cout << "To start finding the safest driving region we need you to provide the number of accidents that happened in each region this past year." << endl;
+    cout << "The names of the city's regions are, North, East, South, West, and Central.\n";
+    for (int i = 0; i < 5; i++) {
+        bool isLowest;
+
+        getRegInfo(regName[i], accNum[i]);
+        lowestReg = regName[0];
+        lowestAcc = accNum[0];
+        cout << regName[i] << " " << accNum[i] << endl; //Making sure calling getRegInfo works correctly
+        isLowest = isLower(accNum[i],lowestAcc);
+        if (isLowest == true) {
+            lowestReg = regName[i];
+            lowestAcc = accNum[i];
+        }
+    }
+    showLowest(lowestReg, lowestAcc);
+    /*islowest = isLower(1, 2); //Concept for what the check for the region with the lowest accidents should look like
+    if (islowest == true) {
+        lowestReg = "";
+        lowestAcc = 1;
+    }
+    showLowest(lowestReg, lowestAcc); //With test data, should include "Central" and "10"
+    return 0;
+    */
+    return 0;
 }
 
 int getRegInfo(string &regName, int &accNum) {
-    cout << "Which region would you like to update?" << endl;
+    cout << "What is the name of the region?" << endl;
     cin >> regName;
-    cout << "How many accidents have happened in the region in the past year?" << endl;
-    cin >> accNum;
-    cin.clear();
-    cin.ignore();
+    while (accNum <= 0) {
+        cout << "How many accidents have happened in the region in the past year?" << endl;
+        cin >> accNum;
+        cin.clear();
+        cin.ignore();
+        if (accNum <= 0) {
+            cout << "Invalid input, please try again." << endl;
+        }
+    }
+    cout << "Input validated." << endl;
     return regName, accNum;
 }
 
@@ -72,5 +109,5 @@ bool isLower(int region1, int region2) {
 }
 
 void showLowest(string name, int accNum) {
-    cout << "The safest region to drive in is " << name << "with " << accNum << " accidents." << endl;
+    cout << "The safest region to drive in is " << name << " with " << accNum << " accidents this past year." << endl;
 }
